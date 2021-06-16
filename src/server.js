a3f66604da60f6b9ff2c5fc628346f575de8e366
Adham <muzaffarov.adham@gmail.com>
2021-06-16 10:12:20 +0500

    public added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 //Express
 const express = require('express')
+const path = require('path')
 const app = express()
 
 //Server
@@ -7,7 +8,17 @@ const { PORT, host } = require('./config')
 
 //Controller
 const homeController = require('./controller/homeController')
+const loginController = require('./controller/loginController')
+const registorController = require('./controller/registorController')
+
+//Middleware
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+//Controller
 
 app.get('/', homeController.GET)
+app.get('/login', loginController.GET)
+app.get('/registor', registorController.GET)
 
 app.listen(PORT, ()=>{console.log("server is running on http://" + host + ":" + PORT)})
